1df3ed8a1be4b3c0d8f961ed941efcedbcb24d39
LuAosUnity <luao23333@gmail.com>
2023-04-16 12:46:26 +0800

    change the player movement

--- Assets/MyFolder/Scripts/Player_Controller.cs
+++ Assets/MyFolder/Scripts/Player_Controller.cs
@@ -8,12 +8,12 @@ public class Player_Controller : MonoBehaviour
     // private Animator anim;
     public float moveSpeed = 50f; // 控制角色移动速度的变量
 
-    private Rigidbody rb; // 用于控制角色运动的刚体组件
+    private CharacterController characterController; // 用于控制角色运动的刚体组件
 
     void Start()
     {
         // 获取角色对象的刚体组件
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -23,17 +23,18 @@ public class Player_Controller : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         // 计算角色的移动向量
-        Vector3 movement = new Vector3(horizontal, vertical, 0) * moveSpeed * Time.deltaTime;
-        print(movement);
+        Vector3 movement = new Vector3(vertical, 0f, horizontal) * moveSpeed * Time.deltaTime;
+        //print(movement);
 
 
         // 将角色的移动向量应用到刚体组件上
-        rb.MovePosition(transform.position + movement);
+        characterController.Move(movement);
 
         // 如果角色有移动输入，则将角色的朝向设置为移动方向
         if (movement.magnitude > 0)
         {
-            transform.rotation = Quaternion.LookRotation(movement.normalized);
+            movement = new Vector3(-horizontal, 0f, vertical);
+            transform.rotation = Quaternion.LookRotation(movement);
         }
 
 
