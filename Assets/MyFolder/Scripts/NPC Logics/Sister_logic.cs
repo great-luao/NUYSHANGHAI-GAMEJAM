b965f3af41489e137197327a0acd7eddf25be232
LuAosUnity <luao23333@gmail.com>
2023-04-16 15:16:05 +0800

    update the animation

--- Assets/MyFolder/Scripts/NPC Logics/Sister_logic.cs
+++ Assets/MyFolder/Scripts/NPC Logics/Sister_logic.cs
@@ -17,13 +17,14 @@ public class Sister_logic : NPC_logic
         dialogue = total_Dialog.sister_dic[st.state];
         dia_index = 0;
         m_name = "sister";
+        if (st.state >= 3) m_name = "Sister";
         //dialogue = new List<string> {"test first","test second","test third" };
     }
 
     public override void ChangeState()
     {
         base.ChangeState();
-        if (st.state == 0) st.state = 1;
+        if (st.state == 0) { st.state = 1;}
         else if (st.state == 3) st.state = 4;
     }
 
